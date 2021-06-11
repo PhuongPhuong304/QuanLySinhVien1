15ab088358816d693cc24cbcb68efd91a5c69bbf
NguyenThiPhuong <phuongottq304@gmail.com>
2021-06-11 20:30:39 +0700

    first commit

---
+++ src/Model/Sinhvien.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Other
+ */
+public class Sinhvien {
+    private String maSV;
+    private String tenSV;
+    private Date ngaySinh;
+    private boolean gioiTinh;
+    private String diaChi;
+    private String sđt;
+    private String email;
+    private String hinhAnh;
+
+    public Sinhvien() {
+    }
+
+    public Sinhvien(String maSV, String tenSV) {
+        this.maSV = maSV;
+        this.tenSV = tenSV;
+    }
+
+    public Sinhvien(String maSV, String tenSV, Date ngaySinh, boolean gioiTinh, String diaChi, String sđt, String email, String hinhAnh) {
+        this.maSV = maSV;
+        this.tenSV = tenSV;
+        this.ngaySinh = ngaySinh;
+        this.gioiTinh = gioiTinh;
+        this.diaChi = diaChi;
+        this.sđt = sđt;
+        this.email = email;
+        this.hinhAnh = hinhAnh;
+    }
+
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+    public String getTenSV() {
+        return tenSV;
+    }
+
+    public void setTenSV(String tenSV) {
+        this.tenSV = tenSV;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public boolean isGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(boolean gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getSđt() {
+        return sđt;
+    }
+
+    public void setSđt(String sđt) {
+        this.sđt = sđt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHinhAnh() {
+        return hinhAnh;
+    }
+
+    public void setHinhAnh(String hinhAnh) {
+        this.hinhAnh = hinhAnh;
+    }
+    
+  
+   
+}
